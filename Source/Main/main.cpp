a2d1330be327d4909c1c6b85bc6959b839460a8e
chriztheanvill <chriztheanvill@gmail.com>
2023-01-22 19:05:51 -0600

    Check

--- Source/Main/main.cpp
+++ Source/Main/main.cpp
@@ -1,7 +1,7 @@
 /**
  * @file main.cpp
  * @author Cristian Nieto (chriztheanvill@gmail.com)
- * @brief A 2D Game Engine, made in C++
+ * @brief A 2D Game Engine, made in C++ in Fedora
  * @version 0.1
  * @date 2022-14-12
  *
